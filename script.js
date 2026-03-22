6919693f73e78883c8b3a657d5f32aea6b91dbb0
cstyczen27-sudo <Cstyczen27@adrian.edu>
2026-03-22 00:20:20 -0400

    added javascript code

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("JS file is connected");
+
+let title = document.getElementById("question");
+let awesome = document.getElementById("type_of_awesome");
+
+console.log(title);
+console.log(awesome);
+
+awesome.textContent = "this is the updated text awesome it is so cool that it updated"
