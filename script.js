1d8c3444b567cbf3e1f2a72b9dcb3ea808ce5d72
cstyczen27-sudo <Cstyczen27@adrian.edu>
2026-03-22 21:25:02 -0400

    adding final touches to quiz practice

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("JS file is connected");
+console.log("is my JS file still connected?");
 
 let title = document.getElementById("question");
 let awesome = document.getElementById("type_of_awesome");
@@ -6,13 +6,31 @@ let awesome = document.getElementById("type_of_awesome");
 console.log(title);
 console.log(awesome);
 
-let buttons = document.querySelectorAll("#type_of_awesome");
+let buttons = document.querySelectorAll("#type_of_awesome button");
 console.log(buttons);
 
+let userAnswers = {}
 buttons.forEach(function(button){
     button.addEventListener("click", function(){
 
-        let output = document.getElementById("output_message"); 
-        output.textContent = "wow that is awesome";
-})
-})
+        let buttonID = button.dataset.buttonid
+        let response = button.dataset.awesome;
+  
+        userAnswers[buttonID] = response;
+        console.log(userAnswers);
+
+        let awesome = button.getAttribute("data-awesome");
+        console.log(awesome)
+        let output = document.getElementById("output-message");
+
+        if(awesome === "selfawesome") {
+            output.textContent = "this is the awseome quiz it is super cool and will tell you a lot about yourself";
+        } else if (awesome === "quizofawesome") {
+            output.textContent = "this is the quiz of awseome it will show you how to be awseome and how not to be awesome";
+        } else if (awesome === "whatawesome") {
+            output.textContent = "this is the quiz about awseome it will tell you what is awesome and what isn't";
+        } else if (awesome === "superawesome") {
+            output.textContent = "this is the super awseome quiz it will tell you what it takes to be super awesome";
+        }
+    });
+});
