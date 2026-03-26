92b681f0a51db05c49f733bf1f4cf98312bc7470
cstyczen27-sudo <Cstyczen27@adrian.edu>
2026-03-26 00:24:19 -0400

    fixing functions and saving changes

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 console.log("is my JS file still connected?");
 
 let title = document.getElementById("question");
-let awesome = document.getElementById("type_of_awesome");
+let awesome = document.getElementById("question_1");
 
 console.log(title);
 console.log(awesome);
 
-let buttons = document.querySelectorAll("#type_of_awesome button");
+let buttons = document.querySelectorAll("#question_1 button");
 console.log(buttons);
 
 let userAnswers = {}
 buttons.forEach(function(button){
     button.addEventListener("click", function(){
+
+        buttons.forEach(function(btn) {
+            btn.classList.remove("selected"); 
+    });
+        button.classList.add("selected");
 
         let buttonID = button.dataset.buttonid
         let response = button.dataset.awesome;
@@ -27,7 +32,7 @@ buttons.forEach(function(button){
             output.textContent = "this is the awseome quiz it is super cool and will tell you a lot about yourself";
         } else if (awesome === "quizofawesome") {
             output.textContent = "this is the quiz of awseome it will show you how to be awseome and how not to be awesome";
-        } else if (awesome === "whatawesome") {
+        } else if (awesome === "whataweseome") {
             output.textContent = "this is the quiz about awseome it will tell you what is awesome and what isn't";
         } else if (awesome === "superawesome") {
             output.textContent = "this is the super awseome quiz it will tell you what it takes to be super awesome";
