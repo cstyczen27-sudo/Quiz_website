c20cbd2758d359ec3a9ecd710f2cb77a1a5cc97a
cstyczen27-sudo <Cstyczen27@adrian.edu>
2026-04-07 15:26:35 -0400

    finalizing js code

--- script.js
+++ script.js
@@ -8,20 +8,16 @@ let Q4 = document.getElementById("question_4")
 let Q5 = document.getElementById("question_5")
 
 console.log(title);
-console.log(Q1);
-
-
-
 let userAnswers = {}
 
 let buttonsQ1 = document.querySelectorAll("#question_1 button");
-buttonsQ1.forEach(function(button) {
-    button.addEventListener("click", function() {
+buttonsQ1.forEach(function(button){
+    button.addEventListener("click", function(){
 
         buttonsQ1.forEach(function(btn) {
-            btn.classList.remove("selected");
+            btn.classList.remove("selected"); 
         });
-
+        
         button.classList.add("selected");
 
         let value = parseInt(button.dataset.awesome);
@@ -32,13 +28,13 @@ buttonsQ1.forEach(function(button) {
 });
 
 let buttonsQ2 = document.querySelectorAll("#question_2 button");
-buttonsQ2.forEach(function(button) {
-    button.addEventListener("click", function() {
+buttonsQ2.forEach(function(button){
+    button.addEventListener("click", function(){
 
         buttonsQ2.forEach(function(btn) {
-            btn.classList.remove("selected");
+            btn.classList.remove("selected"); 
         });
-
+        
         button.classList.add("selected");
 
         let value = parseInt(button.dataset.awesome);
@@ -49,13 +45,13 @@ buttonsQ2.forEach(function(button) {
 });
 
 let buttonsQ3 = document.querySelectorAll("#question_3 button");
-buttonsQ3.forEach(function(button) {
-    button.addEventListener("click", function() {
+buttonsQ3.forEach(function(button){
+    button.addEventListener("click", function(){
 
         buttonsQ3.forEach(function(btn) {
-            btn.classList.remove("selected");
+            btn.classList.remove("selected"); 
         });
-
+        
         button.classList.add("selected");
 
         let value = parseInt(button.dataset.awesome);
@@ -65,15 +61,14 @@ buttonsQ3.forEach(function(button) {
     });
 });
 
-
 let buttonsQ4 = document.querySelectorAll("#question_4 button");
-buttonsQ4.forEach(function(button) {
-    button.addEventListener("click", function() {
+buttonsQ4.forEach(function(button){
+    button.addEventListener("click", function(){
 
         buttonsQ4.forEach(function(btn) {
-            btn.classList.remove("selected");
+            btn.classList.remove("selected"); 
         });
-
+        
         button.classList.add("selected");
 
         let value = parseInt(button.dataset.awesome);
@@ -83,15 +78,14 @@ buttonsQ4.forEach(function(button) {
     });
 });
 
-
 let buttonsQ5 = document.querySelectorAll("#question_5 button");
-buttonsQ5.forEach(function(button) {
-    button.addEventListener("click", function() {
+buttonsQ5.forEach(function(button){
+    button.addEventListener("click", function(){
 
         buttonsQ5.forEach(function(btn) {
-            btn.classList.remove("selected");
+            btn.classList.remove("selected"); 
         });
-
+        
         button.classList.add("selected");
 
         let value = parseInt(button.dataset.awesome);
@@ -100,7 +94,6 @@ buttonsQ5.forEach(function(button) {
         checkTotal();
     });
 });
-
 function checkTotal() {
     console.log("Current answers:", userAnswers);
 
@@ -112,19 +105,19 @@ function checkTotal() {
 
         if (total === 5)  { output.textContent = "Your Stand is: Stone Free"; }
         else if (total === 6)  { output.textContent = "Your Stand is: Star Platinum"; }
-        else if (total === 7)  { output.textContent = "Your Stand is: Wonder of U"; }
-        else if (total === 8)  { output.textContent = "Your Stand is: C-Moon"; }
-        else if (total === 9)  { output.textContent = "Your Stand is: Burning Down The House"; }
-        else if (total === 10) { output.textContent = "Your Stand is: Diver Down"; }
-        else if (total === 11) { output.textContent = "Your Stand is: Tusk"; }
-        else if (total === 12) { output.textContent = "Your Stand is: Dirty Deeds Done Dirt Cheap"; }
-        else if (total === 13) { output.textContent = "Your Stand is: Made in Heaven"; }
-        else if (total === 14) { output.textContent = "Your Stand is: The World"; }
-        else if (total === 15) { output.textContent = "Your Stand is: Killer Queen"; }
-        else if (total === 16) { output.textContent = "Your Stand is: Echos"; }
-        else if (total === 17) { output.textContent = "Your Stand is: King Crimson"; }
-        else if (total === 18) { output.textContent = "Your Stand is: Golden Experience"; }
-        else if (total === 19) { output.textContent = "Your Stand is: Sticky Fingers"; }
-        else if (total === 20) { output.textContent = "Your Stand is: Moody Blues"; }
+        else if (total === 7)  { output.textContent = "Your Stand is: Tusk"; }
+        else if (total === 8)  { output.textContent = "Your Stand is: Killer Queen"; }
+        else if (total === 9)  { output.textContent = "Your Stand is: Soft and Wet"; }
+        else if (total === 10) { output.textContent = "Your Stand is: Sticky Fingers"; }
+        else if (total === 11) { output.textContent = "Your Stand is: The Wrold"; }
+        else if (total === 12) { output.textContent = "Your Stand is: Eclipse 8"; }
+        else if (total === 13) { output.textContent = "Your Stand is: Echos"; }
+        else if (total === 14) { output.textContent = "Your Stand is: Love Deluxe"; }
+        else if (total === 15) { output.textContent = "Your Stand is: Dirty Deeds Done Dirt Cheap"; }
+        else if (total === 16) { output.textContent = "Your Stand is: Golden Experience"; }
+        else if (total === 17) { output.textContent = "Your Stand is: C-Moon"; }
+        else if (total === 18) { output.textContent = "Your Stand is: Made in Heaven"; }
+        else if (total === 19) { output.textContent = "Your Stand is: Moody Blues"; }
+        else if (total === 20) { output.textContent = "Your Stand is: Crazy Diamond"; }
     }
 }
